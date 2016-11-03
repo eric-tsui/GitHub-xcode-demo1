c976e42608c290bc6a4f6c8704f5488d8581f5ab
sen <sensenxu@gmail.com>
2016-11-04 08:15:04 +1100

    Change to git hub 3 now

--- helloworld/ViewController.swift
+++ helloworld/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("Hello there, git hub2!")
+        print("Hello there, git hub3!")
     }
 
     override func didReceiveMemoryWarning() {
